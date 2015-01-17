cf6048cf3b18fceaf4ef4a20cc32c73558577c70
Bastien Andru <bastien.andru@gmail.com>
2015-01-17 18:43:33 +0100

    AJout du generateur de labyrinthe

---
+++ src/fr/labycraft/Modele/Cellule.java
@@ -0,0 +1,22 @@
+package fr.labycraft.Modele;
+
+/**
+ *
+ * @author Bastien Andru <bastien.andru@gmail.com>
+ */
+public class Cellule {
+
+	boolean laby = false;
+	
+	/**
+	 * Default constructor of <code>Cellule</code>.
+	 */
+	public Cellule() {
+		
+	}
+
+	public void setLaby(boolean laby) {
+		this.laby = laby;
+	}
+	
+}
